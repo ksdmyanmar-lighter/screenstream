ab58ce41ae580c0ab99131722b77dffdf7073dac
ksdmyanmar-lighter <ksd_founder@icloud.com>
2026-02-26 23:45:51 +0630

    Update AdMob.kt

--- app/src/FDroid/java/info/dvkr/screenstream/AdMob.kt
+++ app/src/FDroid/java/info/dvkr/screenstream/AdMob.kt
@@ -1,14 +1,14 @@
-	package info.dvkr.screenstream
+#package info.dvkr.screenstream
 
 #import android.app.Activity
 #import android.content.Context
 #import androidx.compose.runtime.Composable
 #import androidx.compose.ui.Modifier
+/*
+public class AdMob(context: Context) {
+    public val isPrivacyOptionsRequired: #Boolean = false
 
-#public class AdMob(context: Context) {
-    #public val isPrivacyOptionsRequired: #Boolean = false
-
-    #public fun showPrivacyOptionsForm(activity: Activity) {}
+    public fun showPrivacyOptionsForm(activity: Activity) {}
 }
 
 @Composable
